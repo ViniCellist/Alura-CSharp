6b08452c51a36828a7f6b1d447f4873509c426d6
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-04-24 15:51:21 -0300

    screen sound project

---
+++ Music.cs
@@ -0,0 +1,25 @@
+class Music
+{
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public int Duration { get; set; }
+    public bool Available { get; set; }
+
+    public string Description =>
+        $"A música {Name} percente à banda { Artist }";
+
+    public void ShowTechDetail()
+    {
+        Console.WriteLine($"Nome: {Name}");
+        Console.WriteLine($"Artista: {Artist}");
+        Console.WriteLine($"Duration: {Duration}");
+        if ( Available )
+        {
+            Console.WriteLine("Disponível no plano.");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        };
+    };
+    
+};
---
+++ Program.cs
@@ -0,0 +1,14 @@
+Music music1 = new Music();
+music1.Name = "Numb";
+music1.Artist = "Linkin Park";
+music1.Duration = 273;
+music1.Available = true;
+
+Music music2 = new Music();
+music2.Name = "Before I forget";
+music2.Artist = "Slipknot";
+music2.Duration = 367;
+music2.Available = false;
+
+music1.ShowTechDetail();
+music2.ShowTechDetail();
